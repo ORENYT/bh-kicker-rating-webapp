7489019096faa3b9c719d431c7f2b10bd0643d38
ORENYT <106102798+ORENYT@users.noreply.github.com>
2023-10-01 21:22:52 +0300

    Created models and user model

--- kicker/models.py
+++ kicker/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+
+class Player(AbstractUser):
+    rating = models.IntegerField()
+    is_pro = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "player"
+        verbose_name_plural = "players"
+
+
+class Game(models.Model):
+    player1 = models.ForeignKey(Player, on_delete=models.SET_NULL,
+                                null=True,  related_name="first_player_games")
+    player2 = models.ForeignKey(Player, on_delete=models.SET_NULL,
+                                null=True,  related_name="second_player_games")
+    player_one_score = models.IntegerField()
+    player2_score = models.IntegerField()
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    working_hours = models.TextField()
+
+
+class Match(models.Model):
+    games = models.ManyToManyField(Game)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["date"]
--- kicker/urls.py
+++ kicker/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from views import index
+from kicker.views import index
 
 urlpatterns = [
     path("", index, name="index")
