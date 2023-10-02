ba7e31556b7e206c0ea540acfb65fc36827b9716
ORENYT <106102798+ORENYT@users.noreply.github.com>
2023-10-02 21:14:24 +0300

    Fixed location referee, added button to create matches, changed default page

--- kicker/admin.py
+++ kicker/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from kicker.models import Location
+from kicker.models import Location, Player
 
 admin.site.register(Location)
+admin.site.register(Player)
