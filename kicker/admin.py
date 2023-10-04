eadc59a7b634f8beb8090ade7b6dce71e3f907be
ORENYT <106102798+ORENYT@users.noreply.github.com>
2023-10-04 15:23:56 +0300

    Changed requirements.txt and settings to work with staticfiles

--- kicker/admin.py
+++ kicker/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from kicker.models import Location, Player, Match
 
-admin.site.register(Location)
 admin.site.register(Player)
 admin.site.register(Match)
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(LocationAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields["admins"].required = False
+        return form
--- kicker_service/settings.py
+++ kicker_service/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -44,12 +46,9 @@ INSTALLED_APPS = [
     "bootstrap4",
 ]
 
-if DEBUG:
-    INSTALLED_APPS += ["debug_toolbar"]
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -57,6 +56,10 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+if DEBUG:
+    INSTALLED_APPS += ["debug_toolbar"]
+    MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
 
 ROOT_URLCONF = "kicker_service.urls"
 
@@ -87,6 +90,10 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# dj-database-url
+db_from_env = dj_database_url.config(conn_max_age=500)  # DATABASE_URL
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -126,6 +133,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATIC_ROOT = "staticfiles/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -140,3 +149,4 @@ INTERNAL_IPS = [
 LOGIN_REDIRECT_URL = "/table/"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
